666f55dd5af5bbe0ba867de8d360b13f7a13dd18
Vale <oirelav95@gmail.com>
2022-11-25 13:10:25 +0100

    aggiungo titolo e immagine

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,14 @@
 // Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const {createApp } = Vue;
+
+createApp({
+    data() {
+      return {
+        title: "Che bel tramonto",
+        image: "img/tramonto.jpg"
+      }
+    }
+  }).mount('#app')
